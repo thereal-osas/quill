f928cee408f27a5000e12ae3f845d97967f9859c
Osas Abraham <abrahamosas3@gmail.com>
2023-12-31 11:15:10 +0000

    stripe integration

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import {
   ChevronDown,
   ChevronUp,
@@ -17,6 +16,7 @@ import { useToast } from "./ui/use-toast";
 import { useResizeDetector } from "react-resize-detector";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { useState } from "react";
 
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -35,11 +35,11 @@ import PdfFullscreen from "./PdfFullscreen";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-type Props = {
+interface PdfRendererProps {
   url: string;
-};
+}
 
-const PdfRenderer = ({ url }: Props) => {
+const PdfRenderer = ({ url }: PdfRendererProps) => {
   const { toast } = useToast();
 
   const [numPages, setNumPages] = useState<number>();
@@ -70,6 +70,8 @@ const PdfRenderer = ({ url }: Props) => {
     resolver: zodResolver(CustomPageValidator),
   });
 
+  console.log(errors);
+
   const { width, ref } = useResizeDetector();
 
   const handlePageSubmit = ({ page }: TCustomPageValidator) => {
@@ -78,9 +80,9 @@ const PdfRenderer = ({ url }: Props) => {
   };
 
   return (
-    <div className="w-full shadow flex bg-white rounded-md flex-col items-center">
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="gap-[1.5] flex items-center">
+        <div className="flex items-center gap-1.5">
           <Button
             disabled={currPage <= 1}
             onClick={() => {
@@ -132,7 +134,8 @@ const PdfRenderer = ({ url }: Props) => {
             <DropdownMenuTrigger asChild>
               <Button className="gap-1.5" aria-label="zoom" variant="ghost">
                 <Search className="h-4 w-4" />
-                {scale * 100}%<ChevronDown className="h-3 w-3 opacity-50" />
+                {scale * 100}%
+                <ChevronDown className="h-3 w-3 opacity-50" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
@@ -164,7 +167,7 @@ const PdfRenderer = ({ url }: Props) => {
       </div>
 
       <div className="flex-1 w-full max-h-screen">
-        <SimpleBar autoHide={false} className="max-h-[calc100vh-10rem]">
+        <SimpleBar autoHide={false} className="max-h-[calc(100vh-10rem)]">
           <div ref={ref}>
             <Document
               loading={
@@ -175,7 +178,7 @@ const PdfRenderer = ({ url }: Props) => {
               onLoadError={() => {
                 toast({
                   title: "Error loading PDF",
-                  description: "Please try again",
+                  description: "Please try again later",
                   variant: "destructive",
                 });
               }}
@@ -192,6 +195,7 @@ const PdfRenderer = ({ url }: Props) => {
                   key={"@" + renderedScale}
                 />
               ) : null}
+
               <Page
                 className={cn(isLoading ? "hidden" : "")}
                 width={width ? width : 1}
@@ -201,7 +205,7 @@ const PdfRenderer = ({ url }: Props) => {
                 key={"@" + scale}
                 loading={
                   <div className="flex justify-center">
-                    <Loader2 className=" h-6 w-6 animate-spin my-24" />
+                    <Loader2 className="my-24 h-6 w-6 animate-spin" />
                   </div>
                 }
                 onRenderSuccess={() => setRenderedScale(scale)}
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,10 +1,11 @@
 import { trpc } from "@/app/_trpc/client";
 import { INFINTE_QUERY_LIMIT } from "@/config/infinte-query";
 import { Loader2, MessageSquare } from "lucide-react";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import Skeleton from "react-loading-skeleton";
 import Message from "./Message";
 import { ChatContext } from "./ChatContext";
+import { useIntersection } from "@mantine/hooks";
 
 type Props = {
   fileId: string;
@@ -43,6 +44,19 @@ const Messages = ({ fileId }: Props) => {
     ...(messages ?? []),
   ];
 
+  const lastMessageRef = useRef<HTMLDivElement>(null);
+
+  const { ref, entry } = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
+
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages && combinedMessages.length > 0 ? (
@@ -54,6 +68,7 @@ const Messages = ({ fileId }: Props) => {
           if (i === combinedMessages.length - 1) {
             return (
               <Message
+                ref={ref}
                 message={message}
                 isNextMessageSamePerson={isNextMessageSamePerson}
                 key={message.id}
