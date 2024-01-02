a0da564790c0bbd5c5d02933d113d73fa72b078c
Osas Abraham <abrahamosas3@gmail.com>
2024-01-02 09:07:04 +0000

    kinde

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, { params }: any) {
+export const GET = (request: NextRequest, { params }: any) => {
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
-}
+};
